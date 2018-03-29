d1828e6c202714a06047e2b283de6f5d4ca9f6cf
root <root@ip-10-125-101-110.us-west-2.compute.internal>
2018-03-29 10:49:13 +0000

    Add setup file

---
+++ setup.py
@@ -0,0 +1,21 @@
+from setuptools import *
+
+setup(
+    name=NAME,
+    version=about['__version__'],
+    description=DESCRIPTION,
+    long_description=long_description,
+    author=AUTHOR,
+    author_email=EMAIL,
+    python_requires=REQUIRES_PYTHON,
+    url=URL,
+    packages=find_packages('mypackage'),
+    # If your package is a single module, use this instead of 'packages':
+    # py_modules=['mypackage'],
+
+    # entry_points={
+    #     'console_scripts': ['mycli=mymodule:cli'],
+    # },
+    install_requires=REQUIRED,
+    include_package_data=True,
+)
