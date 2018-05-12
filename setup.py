5ec9b62a3ffc0960179f45d0c42760464d3e8500
nilanjanray <roy.nilanjan@gmail.com>
2018-05-12 11:41:53 +0530

    change in setup.py

--- setup.py
+++ setup.py
@@ -1,17 +1,20 @@
 from setuptools import *
+import sys
+
 NAME='deadsnake'
 DESCRIPTION='description'
-
+AUTHOR='Nilanjan'
+EMAIL='roy.nilanjan@gmail.com'
 setup(
     name=NAME,
-    version=about['__version__'],
+    version=sys.version,
     description=DESCRIPTION,
-    long_description=long_description,
+    # long_description=long_description,
     author=AUTHOR,
     author_email=EMAIL,
-    python_requires=REQUIRES_PYTHON,
-    url=URL,
-    #packages=find_packages('mypackage'),
+    # python_requires=REQUIRES_PYTHON,
+    # url=URL,
+    # packages=find_packages('mypackage'),
     # If your package is a single module, use this instead of 'packages':
     py_modules=['memcache_stat'],
 
